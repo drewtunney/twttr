1fc22d191ab85909e774b2cf7eacb48db9c7885d
drewtunney <bryantunney@gmail.com>
2014-06-03 11:20:03 -0400

    user model

--- tweets/models.py
+++ tweets/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class Users(models.Model):
+  username = models.CharField(max_length=50)
 
 
 class Tweets(models.Model):
   message = models.CharField(max_length=140)
   pub_date = models.DateTimeField('date published')
-  # user = models.ForeignKey(User)
+  user = models.ForeignKey('Users')
+
 
 
